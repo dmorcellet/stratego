d83480bc3164a9794cdc2b91b5b30f9be28ed860
Damien Morcellet <dmorcellet@free.fr>
2022-11-23 16:59:27 +0100

    Added a draft main frame controller

---
+++ src/main/java/delta/games/stratego/ui/MainFrameController.java
@@ -0,0 +1,152 @@
+package delta.games.stratego.ui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
+import delta.games.stratego.model.board.Board;
+
+/**
+ * Controller for the Stratego main frame.
+ * @author DAM
+ */
+public class MainFrameController implements ActionListener
+{
+  private JFrame _frame;
+  private static final String NEW_GAME_CMD="NEW_GAME";
+  private static final String QUIT_CMD="QUIT";
+
+  private BoardPanel _boardPanel;
+  private SidePanel _sidePanel;
+
+  /**
+   * Constructor.
+   */
+  public MainFrameController()
+  {
+    // Nothing!
+  }
+
+  /**
+   * Get the managed frame (build it if necessary).
+   * @return A frame.
+   */
+  public JFrame getFrame()
+  {
+    if (_frame==null)
+    {
+      _frame=buildFrame();
+    }
+    return _frame;
+  }
+
+  private JFrame buildFrame()
+  {
+    JFrame frame=new JFrame("Stratego");
+    JMenuBar menus=buildMenus();
+    frame.setJMenuBar(menus);
+    JPanel mainPanel=buildMainPanel();
+    frame.add(mainPanel);
+    frame.pack();
+    return frame;
+  }
+
+  private JMenuBar buildMenus()
+  {
+    // Create the menu bar
+    JMenuBar menuBar=new JMenuBar();
+
+    // Build the "Game" menu
+    JMenu menu=new JMenu("Jeu");
+    menu.setMnemonic(KeyEvent.VK_J);
+    menuBar.add(menu);
+
+    // New game item
+    {
+      JMenuItem menuItem=new JMenuItem("Nouvelle partie",KeyEvent.VK_N);
+      menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,ActionEvent.ALT_MASK));
+      menuItem.setActionCommand(NEW_GAME_CMD);
+      menuItem.addActionListener(this);
+      menu.add(menuItem);
+    }
+    menu.addSeparator();
+    // Quit item
+    {
+      JMenuItem menuItem=new JMenuItem("Quitter",KeyEvent.VK_Q);
+      menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,ActionEvent.ALT_MASK));
+      menuItem.setActionCommand(QUIT_CMD);
+      menuItem.addActionListener(this);
+      menu.add(menuItem);
+    }
+    menuBar.add(menu);
+    return menuBar;
+  }
+
+  private JPanel buildMainPanel()
+  {
+    JPanel mainPanel=new JPanel(new GridBagLayout());
+    Board board=new Board(10,10);
+    _boardPanel=new BoardPanel(board);
+    GridBagConstraints c1=new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0);
+    mainPanel.add(_boardPanel,c1);
+    _sidePanel=new SidePanel();
+    GridBagConstraints c2=new GridBagConstraints(1,0,1,1,0,0,GridBagConstraints.NORTH,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0);
+    mainPanel.add(_sidePanel,c2);
+    return mainPanel;
+  }
+
+  /**
+   * Get the board panel.
+   * @return the board panel.
+   */
+  public BoardPanel getBoardPanel()
+  {
+    return _boardPanel;
+  }
+
+  /**
+   * Show the managed frame.
+   */
+  public void show()
+  {
+    JFrame frame=getFrame();
+    frame.setVisible(true);
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    String cmd=e.getActionCommand();
+    if (NEW_GAME_CMD.equals(cmd))
+    {
+    }
+    else if (QUIT_CMD.equals(cmd))
+    {
+      doQuit();
+    }
+  }
+
+  private void doQuit()
+  {
+    System.exit(0);
+  }
+
+  /**
+   * Main method for the Stratego game.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    MainFrameController controller=new MainFrameController();
+    controller.show();
+  }
+}
---
+++ src/main/java/delta/games/stratego/ui/SidePanel.java
@@ -0,0 +1,69 @@
+package delta.games.stratego.ui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Panel that displays the game status (player name,...).
+ * @author DAM
+ */
+public class SidePanel extends JPanel
+{
+  private static final boolean DEBUG=false;
+  private JLabel _playerName;
+
+  /**
+   * Constructor.
+   * @param game Game to show.
+   */
+  public SidePanel()
+  {
+    super(new GridBagLayout());
+    buildGUI();
+  }
+
+  private void buildGUI()
+  {
+    GridBagConstraints constraints;
+    int x=0,y=0;
+    // Stratego main label
+    {
+      JLabel gameNameLabel=new JLabel("Stratego",JLabel.CENTER);
+      setLabelSize(gameNameLabel,72f);
+      if (DEBUG)
+      {
+        gameNameLabel.setOpaque(true);
+        gameNameLabel.setBackground(Color.RED);
+      }
+      constraints=new GridBagConstraints(x,y,1,1,1.0,0.0,GridBagConstraints.CENTER,GridBagConstraints.HORIZONTAL,new Insets(5,5,5,5),0,0);
+      add(gameNameLabel,constraints);
+    }
+    y++;
+    // Player name
+    {
+      _playerName=new JLabel("Joe",JLabel.CENTER);
+      setLabelSize(_playerName,36f);
+      if (DEBUG)
+      {
+        _playerName.setOpaque(true);
+        _playerName.setBackground(Color.GREEN);
+      }
+      constraints=new GridBagConstraints(x,y,1,1,1.0,0.0,GridBagConstraints.CENTER,GridBagConstraints.HORIZONTAL,new Insets(5,5,5,5),0,0);
+      add(_playerName,constraints);
+    }
+    y++;
+  }
+
+  private void setLabelSize(JLabel label, float size)
+  {
+    Font font=label.getFont();
+    font=font.deriveFont(size);
+    label.setFont(font);
+  }
+}
